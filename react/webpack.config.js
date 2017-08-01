82295f6a4575a73b48648172b3ec33349a3e4abd
Jared Shulman <JaredCapImpact@Jareds-MacBook-Air-3.local>
2017-08-01 16:47:05 -0400

    added config into react folder

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,46 +1,23 @@
-// var webpack = require('webpack');
-//
-// module.exports = {
-//   entry: {
-//     path: './src/main.js'
-//   },
-//   output: {
-//     path: __dirname+'/build',
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.scss$/,
-//         use: [
-//           "style-loader",
-//           "css-loader",
-//           "sass-loader"
-//         ]
-//       },
-//       {
-//         test: /\.jsx?$/,
-//         exclude: /node_modules/,
-//         loader: 'babel-loader'
-//       }
-//     ]
-//   },
-//   devtool: 'eval-source-map',
-//   devServer: {
-//     contentBase: './build',
-//     inline: true
-//   }
-// }
-var config = {
+var webpack = require('webpack');
+
+module.exports = {
   entry: {
-        path: __dirname + '/src/main.js',
+    path: './src/main.js'
   },
   output: {
-        path: __dirname + '/../app/assets/javascripts',
+    path: __dirname+'/build',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -48,15 +25,38 @@ var config = {
       }
     ]
   },
-  devtool: 'eval-source-map'
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './build',
+    inline: true
+  }
 }
-
-if (process.env.NODE_ENV === 'production') {
-  delete config.devtool;
-  var webpack = require('webpack');
-  config.plugins = [
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
-  ];
-}
-
-module.exports = config;
+// var config = {
+//   entry: {
+//         path: __dirname + '/src/main.js',
+//   },
+//   output: {
+//         path: __dirname + '/../app/assets/javascripts',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader'
+//       }
+//     ]
+//   },
+//   devtool: 'eval-source-map'
+// }
+//
+// if (process.env.NODE_ENV === 'production') {
+//   delete config.devtool;
+//   var webpack = require('webpack');
+//   config.plugins = [
+//     new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
+//   ];
+// }
+//
+// module.exports = config;
