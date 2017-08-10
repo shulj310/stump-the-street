a5958c1b0e3dfec0b00d53804385a624cd35ffc9
Jared Shulman <JaredCapImpact@Jareds-MacBook-Air-3.local>
2017-08-09 21:42:14 -0400

    mail gun config rd III

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options =
+      {:host => 'https://stump-the-street.herokuapp.com'}
+
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
