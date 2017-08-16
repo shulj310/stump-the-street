ed66cf6c7ce1468d61da939856a719e494ee99a6
Jared Shulman <JaredCapImpact@Jareds-MacBook-Air-3.local>
2017-08-16 05:10:32 -0400

    removed debugger;

--- app/javascript/react/src/containers/Portfolio.js
+++ app/javascript/react/src/containers/Portfolio.js
@@ -44,7 +44,6 @@ class Portfolio extends Component{
               netCash += (trade.shares * trade.stock.price)
             }
           })
-          debugger;
           this.setState({showTradeQueue:true,netCashQueue:netCash})
         }
         else{
