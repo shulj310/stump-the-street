4d68092b535acef7d257fe39e9c9b875ab3ae9a7
Jared Shulman <JaredCapImpact@Jareds-MacBook-Air-3.local>
2017-08-29 18:30:58 -0400

    update status mailer

--- app/mailers/status_mailer.rb
+++ app/mailers/status_mailer.rb
@@ -1,8 +1,12 @@
 class StatusMailer < ApplicationMailer
 
-  def update(total_users,total_competitions,total_exposure,current_exposure)
+  def update(total_users,total_competitions,total_exposure,last_day_users,latest_users,current_exposure,stock_exposure, sorted_exposure_hash)
 
     @total_users,@total_competitions,@total_exposure, @current_exposure = total_users,total_competitions,total_exposure,current_exposure
+    @last_day_users = last_day_users
+    @latest_users = latest_users
+    @sorted_exposure_hash = sorted_exposure_hash
+    @stock_exposure = stock_exposure
 
     mail(
     to:"shulman.jared@gmail.com",
